3a78bc155c2d87d6727a746d735e6a0403bf3568
Kyan Cox <kyan.cox@gmail.com>
2024-07-01 10:49:54 -0400

    Chatbot working

--- app/(tabs)/chatbot.tsx
+++ app/(tabs)/chatbot.tsx
@@ -1,16 +1,166 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  Platform,
+  LogBox,
+} from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+import { GiftedChat, IMessage } from 'react-native-gifted-chat'
+import { v4 as uuidv4 } from 'uuid';
 
 const Chatbot = () => {
-  // chatscope library
+  const error = console.error; console.error = (...args) => { if (/defaultProps/.test(args[0])) return; error(...args); };
   // https://www.youtube.com/watch?v=Lag9Pj_33hM
+  const [chatStarted, setChatStarted] = useState(false);
 
-  
+  const gptAvatar = require('../../assets/images/gpt-trans.png');
+
+  const systemMessage = {
+    'role': 'system',
+    'content': 'You are to answer all of my questions.'
+  }
+
+  const [messages, setMessages] = useState<IMessage[]>([])
+
+  const test = {
+    'role': 'assistant',
+    'content': `Hello! I'm here to help you.`
+  }
+
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: uuidv4(),
+        text: `Hello! I'm here to help you.`,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'ChatGPT',
+          avatar: gptAvatar,
+        },
+      },
+    ])
+  }, [])
+
+  const fetchOpenAIResponse = async (message: { role: string, content: string }) => {
+    // https://github.com/expo/expo/issues/28933
+    // EXPO_PUBLIC for development, come back and fix in production
+
+    // console.log(messages)
+
+    const apiMessages = messages.slice().reverse().map(message => {
+      return { role: message.user.name === 'ChatGPT' ? 'assistant' : 'user', content: message.text }
+    })
+
+    // console.log(apiMessages)
+
+    // console.log(message)
+
+    const apiRequestBody = {
+      'model': 'gpt-3.5-turbo',
+      'messages': [
+        systemMessage,
+        ...apiMessages,
+        message
+      ]
+    }
+    console.log(`apiRequestMessages: ${JSON.stringify([
+      systemMessage,
+      ...apiMessages,
+      message
+    ],null,2)}`)
+
+    try {
+      const chatCompletion = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${process.env.EXPO_PUBLIC_OPENAI_API_KEY}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(apiRequestBody)
+      });
+
+      if (!chatCompletion.ok) {
+        console.error('Error fetching OpenAI response:', chatCompletion.statusText);
+        return;
+      }
+
+      const response = await chatCompletion.json();
+      // console.log('OpenAI response:', response);
+
+      if (!response.choices || response.choices.length === 0) {
+        console.error('No choices in OpenAI response');
+        return;
+      }
+
+      const message = response.choices[0].message;
+
+      if (!message || !message.content) {
+        console.error('Invalid message format in OpenAI response');
+        return;
+      }
+
+      const formattedChatMessage = {
+        _id: uuidv4(),
+        text: message.content,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'ChatGPT',
+          avatar: gptAvatar,
+        }
+      };
+
+      setMessages(previousMessages =>
+        GiftedChat.append(previousMessages, [formattedChatMessage]),
+      );
+    } catch (error) {
+      console.error('Error in fetchOpenAIResponse:', error);
+    }
+  }
+
+  const onSend = (message: IMessage[] = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, message),
+    )
+    // console.log(`messages: ${JSON.stringify(messages)}`)
+    const formattedApiMessage = {
+      role: 'user',
+      content: message[0].text
+    }
+    // console.log(`formattedApiMessage: ${JSON.stringify(formattedApiMessage)}`)
+    // await setApiMessages(prevState => {
+    //   const newState = [...prevState, formattedApiMessage];
+    //   console.log(`newState: ${JSON.stringify(newState)}`); // Log the new state to verify
+    //   return newState;
+    // });
+    // console.log(`apiMessages: ${JSON.stringify(apiMessages)}`)
+    //
+    fetchOpenAIResponse(formattedApiMessage);
+  };
+
   return (
-    <View>
-      <Text>chatbot</Text>
-    </View>
-  )
-}
+    <>
+      <SafeAreaView>
+        {messages.map(message => (
+          <View key={message._id}> 
+          <Text>{message.text}</Text>
+          </View>
+        ))}
+      </SafeAreaView>
+      <GiftedChat
+        messages={messages}
+        onSend={message => onSend(message)}
+        user={{
+          _id: 1,
+        }}
+        bottomOffset={100}
+      />
+    </>
+  );
+};
 
-export default Chatbot
+export default Chatbot;
--- app/index.tsx
+++ app/index.tsx
@@ -6,8 +6,8 @@ export default function App() {
     return (
         <SafeAreaView className="flex-1 gap-8 items-center justify-center bg-white">
             <Link href="/create" className='text-6xl text-secondary font-extrabold'>Create</Link>
+            <Link href="/chatbot" className='text-6xl text-secondary font-extrabold'>Chatbot</Link>
             <Link href="/community" className='text-6xl text-secondary font-extrabold'>Community</Link>
-            <Link href="/resources" className='text-6xl text-secondary font-extrabold'>Resources</Link>
             <Link href="/resources" className='text-6xl text-secondary font-extrabold'>Resources</Link>
             <Link href="/resources" className='text-6xl text-secondary font-extrabold'>Resources</Link>
         </SafeAreaView>
