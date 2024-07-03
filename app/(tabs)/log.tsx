4898ddbc5ba997b3f5a63ab131e3abc8f7b01f0b
Kyan Cox <kyan.cox@gmail.com>
2024-07-03 17:26:39 -0400

    Log landing page complete

--- app/(tabs)/log.tsx
+++ app/(tabs)/log.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable } from 'react-native'
+import { Link } from "expo-router";
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { FlatList } from 'react-native-reanimated/lib/typescript/Animated'
 
 const Log = () => {
+  const [logs, setLogs] = useState(false)
+
   return (
-    <SafeAreaView>
-      <Text>log</Text>
+    <SafeAreaView
+      className='flex-1'
+    >
+      {!logs && (
+        <View
+          className='h-full flex flex-col items-center justify-center px-2'
+        >
+          <Text className='text-2xl font-extrabold text-center mb-4'>You currently have no VSS logs.</Text>
+
+          <Link href={'/create'} className='' asChild>
+            <Pressable
+              style={{
+                backgroundColor: '#FFA500',
+                padding: 20,
+                borderRadius: 5,
+                elevation: 3,
+              }}
+            >
+              <Text className='text-white text-2xl font-bold'>Make your first log.</Text>
+            </Pressable>
+          </Link>
+
+        </View>)}
     </SafeAreaView>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,10 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 export default function App() {
     return (
         <SafeAreaView className="flex-1 gap-8 items-center justify-center bg-white">
-            <Link href="/create" className='text-6xl text-secondary font-extrabold'>Create</Link>
             <Link href="/chatbot" className='text-6xl text-secondary font-extrabold'>Chatbot</Link>
             <Link href="/community" className='text-6xl text-secondary font-extrabold'>Community</Link>
-            <Link href="/resources" className='text-6xl text-secondary font-extrabold'>Resources</Link>
+            <Link href="/create" className='text-6xl text-secondary font-extrabold'>Create</Link>
+            <Link href="/log" className='text-6xl text-secondary font-extrabold'>Log</Link>
             <Link href="/resources" className='text-6xl text-secondary font-extrabold'>Resources</Link>
         </SafeAreaView>
     )
