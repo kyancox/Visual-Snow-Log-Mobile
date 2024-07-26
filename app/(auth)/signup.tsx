0c7b8b8c3da50a067429b4d200a6e4928eaec529
Kyan Cox <kyan.cox@gmail.com>
2024-07-25 23:19:42 -0400

    Re-enter password complete

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -11,138 +11,159 @@ import Logo from '@/components/Logo'
 // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
 // if the user's session is terminated. This should only be registered once.
 AppState.addEventListener('change', (state) => {
-  if (state === 'active') {
-    supabase.auth.startAutoRefresh()
-  } else {
-    supabase.auth.stopAutoRefresh()
-  }
+    if (state === 'active') {
+        supabase.auth.startAutoRefresh()
+    } else {
+        supabase.auth.stopAutoRefresh()
+    }
 })
 
 export default function Auth() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [emailFocused, setEmailFocused] = useState(false)
-  const [passwordFocused, setPasswordFocused] = useState(false)
-  const [secondPassword, setSecondPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [emailFocused, setEmailFocused] = useState(false)
+    const [passwordFocused, setPasswordFocused] = useState(false)
+    const [secondPassword, setSecondPassword] = useState('')
+    const [secondPasswordFocused, setSecondPasswordFocused] = useState(false)
 
-  async function signInWithEmail() {
-    setLoading(true)
-    const { error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    })
+    async function signInWithEmail() {
+        setLoading(true)
+        const { error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
 
-    if (error) Alert.alert(error.message)
-    setLoading(false)
-  }
+        if (error) Alert.alert(error.message)
+        setLoading(false)
+    }
 
-  async function signUpWithEmail() {
-    setLoading(true)
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    })
+    async function signUpWithEmail() {
+        if (!email) {
+            Alert.alert('Please enter an email!')
+            return
+        }
+        if (!password) {
+            Alert.alert('Please enter a password!')
+            return
+        }
 
-    if (error) Alert.alert(error.message)
-    else if (!session) Alert.alert('Please check your inbox for email verification!')
-    setLoading(false)
-  }
+        if (password !== secondPassword) {
+            Alert.alert('Passwords do not match!')
+            return
+        }
 
-  return (
-    <SafeAreaView className='bg-white h-full'>
+        setLoading(true)
+        const {
+            data: { session },
+            error,
+        } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        })
 
+        if (error) Alert.alert(error.message)
+        else if (!session) Alert.alert('Please check your inbox for email verification!')
+        setLoading(false)
+    }
 
-      <View className='mx-auto w-10/12 space-y-5 my-auto'>
-        <Logo />
-        <View className='flex flex-col'>
-          <Text className='text-2xl mb-1 font-psemibold'>Email</Text>
-          <TextInput
-            className={`bg-gray-100 p-3 border rounded-lg ${emailFocused ? 'border-projectOrange' : 'border-white'}`}
-            onChangeText={(text) => setEmail(text)}
-            value={email}
-            placeholder="email@address.com"
-            autoCapitalize={'none'}
-            onFocus={() => setEmailFocused(true)}
-            onBlur={() => setEmailFocused(false)}
-          />
-        </View>
-
-
-        <View>
-          <Text className='text-2xl mb-1 font-psemibold'>Password</Text>
-          <TextInput
-            className={`bg-gray-100 p-3 border rounded-lg ${passwordFocused ? 'border-projectOrange' : 'border-white'}`}
-            onChangeText={(text) => setPassword(text)}
-            value={password}
-            secureTextEntry={true}
-            placeholder="Password"
-            autoCapitalize={'none'}
-            onFocus={() => setPasswordFocused(true)}
-            onBlur={() => setPasswordFocused(false)}
-          />
-        </View>
-
-        <View>
-          <Text className='text-2xl mb-1 font-psemibold'>Re-enter Password</Text>
-          <TextInput
-            className={`bg-gray-100 p-3 border rounded-lg ${secondPassword === password ? 'border-projectOrange' : 'border-red'}`}
-            onChangeText={(text) => setPassword(text)}
-            value={secondPassword}
-            secureTextEntry={true}
-            placeholder="Password"
-            autoCapitalize={'none'}
-            onFocus={() => setPasswordFocused(true)}
-            onBlur={() => setPasswordFocused(false)}
-          />
-        </View>
+    return (
+        <SafeAreaView className='bg-white h-full'>
 
 
-        <TouchableOpacity className=' bg-projectOrange rounded-lg flex p-2 flex-row items-center justify-center'
-          onPress={() => signUpWithEmail()}
-          disabled={loading}
-        >
-          <Text className='text-white font-bold text-center text-lg'>Sign up</Text>
-        </TouchableOpacity>
+            <View className='mx-auto w-10/12 space-y-5 my-auto'>
+                <Logo />
+                <View className='flex flex-col'>
+                    <Text className='text-2xl mb-1 font-psemibold'>Email</Text>
+                    <TextInput
+                        className={`bg-gray-100 p-3 border rounded-lg ${emailFocused ? 'border-projectOrange' : 'border-white'}`}
+                        onChangeText={(text) => setEmail(text)}
+                        value={email}
+                        placeholder="email@address.com"
+                        autoCapitalize={'none'}
+                        onFocus={() => setEmailFocused(true)}
+                        onBlur={() => setEmailFocused(false)}
+                    />
+                </View>
 
-        <View className='mx-auto mt-auto flex flex-row items-center justify-center '>
-                <Text className=''>Already have an account?</Text>
-                <Pressable
-                    onPress={() => router.push('/login')}
+
+                <View>
+                    <Text className='text-2xl mb-1 font-psemibold'>Password</Text>
+                    <TextInput
+                        className={`bg-gray-100 p-3 border rounded-lg ${passwordFocused ? 'border-projectOrange' : 'border-white'}`}
+                        onChangeText={(text) => setPassword(text)}
+                        value={password}
+                        secureTextEntry={true}
+                        placeholder="Password"
+                        autoCapitalize={'none'}
+                        onFocus={() => setPasswordFocused(true)}
+                        onBlur={() => setPasswordFocused(false)}
+                    />
+                </View>
+
+                <View>
+                    <Text className='text-2xl mb-1 font-psemibold'>Re-enter Password</Text>
+                    <TextInput
+                        className={`bg-gray-100 p-3 border rounded-lg ${secondPasswordFocused ? 'border-projectOrange' : 'border-white'}`}
+                        onChangeText={(text) => setSecondPassword(text)}
+                        value={secondPassword}
+                        secureTextEntry={true}
+                        placeholder="Password"
+                        autoCapitalize={'none'}
+                        onFocus={() => setSecondPasswordFocused(true)}
+                        onBlur={() => setSecondPasswordFocused(false)}
+                    />
+                </View>
+
+                {password !== secondPassword &&
+                    <Text className='text-red-500 text-center'>
+                        Please make sure both passwords are the same.
+                    </Text>
+                }
+
+
+                <TouchableOpacity className=' bg-projectOrange rounded-lg flex p-2 flex-row items-center justify-center'
+                    onPress={() => signUpWithEmail()}
+                    disabled={loading}
                 >
-                    <Text className='font-bold text-projectOrange'> Sign in</Text>
-                </Pressable>
+                    <Text className='text-white font-bold text-center text-lg'>Sign up</Text>
+                </TouchableOpacity>
+
+                <View className='mx-auto mt-auto flex flex-row items-center justify-center '>
+                    <Text className=''>Already have an account?</Text>
+                    <Pressable
+                        onPress={() => router.push('/login')}
+                    >
+                        <Text className='font-bold text-projectOrange'> Sign in</Text>
+                    </Pressable>
+                </View>
+
             </View>
 
-      </View>
+            <Pressable className='mt-auto mx-auto'
+                onPress={() => {
+                    supabase.auth.signInAnonymously()
+                    router.push('/create')
+                }}
+            >
+                <Text className='text-projectOrange'>Continue as Guest</Text>
+            </Pressable>
 
-      <Pressable className='mt-auto mx-auto'
-      onPress={() => {
-        supabase.auth.signInAnonymously()
-        router.push('/create')
-      }}
-      >
-        <Text className='text-projectOrange'>Continue as Guest</Text>
-      </Pressable>
-
-    </SafeAreaView>
-  )
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    padding: 12,
-  },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: 'stretch',
-  },
-  mt20: {
-    marginTop: 20,
-  },
+    container: {
+        marginTop: 40,
+        padding: 12,
+    },
+    verticallySpaced: {
+        paddingTop: 4,
+        paddingBottom: 4,
+        alignSelf: 'stretch',
+    },
+    mt20: {
+        marginTop: 20,
+    },
 })
